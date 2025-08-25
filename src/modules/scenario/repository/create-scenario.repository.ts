e14f3f1451a30f5361f56e178fb197f45deb8326
GustavoRdps <gustavosilva.bexidas@gmail.com>
2025-08-25 10:55:32 -0400

    commit 1.1

---
+++ src/modules/scenario/repository/create-scenario.repository.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/shared/databases/prisma.database";
+import { CreateScenarioDto } from "../dto/create-scenario.dto";
+
+@Injectable()
+export class CreateScenarioRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateScenarioDto) {
+    const scenario = await this.prisma.scenario.create({
+        data,
+    });
+    return scenario;
+  }
+}
